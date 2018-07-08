ca531d0cb8468b9f93c09630d787672f56ca0c83
Ugochukwu Aronu <ugochukwuaronu@gmail.com>
2018-07-08 06:28:53 +0100

    Test Commit

--- UgoChain/Program.cs
+++ UgoChain/Program.cs
@@ -6,7 +6,7 @@ namespace UgoChain
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("HelloBlockchain!");
         }
     }
 }
