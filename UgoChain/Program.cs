ac6c0d581e535169eff308753e7077263eb0ad48
Ugochukwu Aronu <ugochukwuaronu@gmail.com>
2018-07-08 05:58:59 +0100

    Add project files.

---
+++ UgoChain/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace UgoChain
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
