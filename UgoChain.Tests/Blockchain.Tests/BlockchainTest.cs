5a1609a74c890a52bc557354e2f82b184f85fd55
Ugochukwu Aronu <ugochukwuaronu@gmail.com>
2018-07-09 11:17:26 +0100

    Chain validation and tests

--- UgoChain.Tests/Blockchain.Tests/BlockchainTest.cs
+++ UgoChain.Tests/Blockchain.Tests/BlockchainTest.cs
@@ -10,7 +10,7 @@ namespace UgoChain.Tests
     public class BlockchainTest
     {
         private Features.Blockchain _blockchain { get; set; } = new Features.Blockchain();
-
+        private Features.Blockchain _blockchain2 { get; set; } = new Features.Blockchain();
         /// <summary>
         /// Blockchain should start with genesis block
         /// </summary>
@@ -31,6 +31,42 @@ namespace UgoChain.Tests
             Block freshBlock =_blockchain.AddBlock(data);
             Assert.Equal(_blockchain.Chain.LastOrDefault().Data, data);
 
+        }
+        /// <summary>
+        /// Validates a valid chain
+        /// 1. Add a new block to the _blockchain2 instance and test its 
+        /// validity with the  _blockchain instance
+        /// </summary>
+        [Fact]
+        public void ShoudValidateValidChain()
+        {
+            //Test 1
+            _blockchain2.AddBlock("New block on second blockchain");
+
+            Assert.True(_blockchain.isChainValid(_blockchain2));
+
+        }
+
+        /// <summary>
+        /// Invalids a chain with a corrupt genesis block
+        /// </summary>
+        [Fact]
+        public void ShouldInvalidateChainWithCorruptGenesisBlock()
+        {
+            _blockchain2.Chain[0].Data = "I am changing the genesis data. Haha!!";
+            Assert.False(_blockchain.isChainValid(_blockchain2));
+        }
+
+        /// <summary>
+        /// Invalidates a corrupt chain that is not the genesis block
+        /// </summary>
+        [Fact]
+        public void InvalidatesCorruptChain()
+        {
+            _blockchain2.AddBlock("Good data :)");
+            _blockchain2.Chain[1].Data = "Corrupting the good data :(";
+
+            Assert.False(_blockchain.isChainValid(_blockchain2));
         }
     }
 }
--- UgoChain/Features/Blockchain.cs
+++ UgoChain/Features/Blockchain.cs
@@ -26,5 +26,30 @@ namespace UgoChain.Features
             Chain.Add(freshBlock);
             return freshBlock;
         }
+
+        /// <summary>
+        /// 1. Checks if the first block is the genesis block
+        /// 2. Checks if the current block's last hash property is equal to the last block's hash property
+        /// 3. Checks if data manipulation has occcured by rebuilding the current block's hash with the Blocks HashFunction
+        /// and checking if the two hashes are equal
+        /// </summary>
+        /// <param name="blockchain"></param>
+        /// <returns></returns>
+        public bool isChainValid(Blockchain blockchain)
+        {
+            if (!blockchain.Chain.FirstOrDefault().Equals(Block.GenesisBlock()))
+                return false;
+
+            for (int i = 1; i < blockchain.Chain.Count; i++)
+            {
+                Block currentBlock = blockchain.Chain[i];
+                Block lastBlock = blockchain.Chain[i - 1];
+                if (currentBlock.LastHash != lastBlock.Hash || currentBlock.Hash != Block.BlockHash(currentBlock))
+                    return false;
+                
+            }
+
+            return true;
+        }
     }
 }
