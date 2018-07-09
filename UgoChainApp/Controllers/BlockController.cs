5269777e7463c445865a32993f614140529b1424
rony4d <rony4d@gmail.com>
2018-07-09 19:41:10 +0100

    GetBlocks Api

---
+++ UgoChainApp/Controllers/BlockController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using UgoChain.Features;
+
+namespace UgoChainApp.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/block")]
+    public class BlockController : Controller
+    {
+        [HttpGet("getblocks")]
+        public IActionResult GetBlocks()
+        {
+            Blockchain blockchain = new Blockchain();
+
+            return Ok(blockchain.Chain);
+        }
+    }
+}
