aac6a99e4b6033f1eda07ff02c8cf067781f406d
Ugochukwu Aronu <ugochukwuaronu@gmail.com>
2018-07-10 05:55:56 +0100

    .net core 2.1 added

---
+++ UgoChain.Api/Controllers/BlockController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using UgoChain.Features;
+
+namespace UgoChain.Api.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/[controller]")]
+    public class BlockController : Controller
+    {
+
+        Blockchain blockchain { get; set; } = new Blockchain();
+        [HttpGet("getblocks")]
+        public IActionResult GetBlocks()
+        {
+
+            return Ok(blockchain.Chain);
+        }
+
+        [HttpPost("mine")]
+        public IActionResult Mine(Block block)
+        {
+            blockchain.AddBlock(block.Data);
+            return Ok(blockchain.Chain);
+        }
+    }
+}
